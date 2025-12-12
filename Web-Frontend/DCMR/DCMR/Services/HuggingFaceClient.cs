3e0d036d44e041c30a55036dab18d89d991aae0e
ave-the <thomas.ewald@aventit.com>
2025-12-12 20:46:02 +0100

    Added Ingredient & Recipe page after project restructuring

--- Web-Frontend/DCMR/DCMR/Services/HuggingFaceClient.cs
+++ Web-Frontend/DCMR/DCMR/Services/HuggingFaceClient.cs
@@ -3,6 +3,7 @@ namespace DCMR.Services;
 using System.Text.Json;
 using System.Net.Http.Headers;
 using System.Text;
+using DCMR.Models;
 
 
 public class HuggingFaceClient : IHuggingFaceClient
@@ -38,6 +39,37 @@ public class HuggingFaceClient : IHuggingFaceClient
 
         response.EnsureSuccessStatusCode();
 
+
+        var json = await response.Content.ReadAsStringAsync();
+        return json;
+    }
+public async Task<string> GenerateImageAsync(List<Ingredient> ingredients)
+    {
+        return await GetRecipeAsync(ingredients);
+    }
+    public async Task<string> GetRecipeAsync(List<Ingredient> ingredients)
+    {
+        var prompt = "A high quality food photo of a dish made with: " +
+                     string.Join(", ", ingredients.Select(i => i.Name)) +
+                     ". Highly detailed, professional lighting, 8k resolution.";
+
+        var request = new
+        {
+            inputs = prompt,
+            parameters = new
+            {
+                width = 512,
+                height = 512,
+                num_inference_steps = 50
+            }
+        };
+
+        var response = await _client.PostAsJsonAsync(
+            "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4",
+            request
+        );
+
+        response.EnsureSuccessStatusCode();
 
         var json = await response.Content.ReadAsStringAsync();
         return json;
--- Web-Frontend/DCMR/DCMR/Services/IHuggingFaceClient.cs
+++ Web-Frontend/DCMR/DCMR/Services/IHuggingFaceClient.cs
@@ -1,6 +1,10 @@
+using DCMR.Models;
+
 namespace DCMR.Services;
 
 public interface IHuggingFaceClient
 {
     Task<string> QueryAsync(string prompt);
+
+    Task<string> GetRecipeAsync(List<Ingredient> ingredients);
 }
